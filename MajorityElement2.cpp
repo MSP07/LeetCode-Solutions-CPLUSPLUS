8d1d6ccc8f344cb6edc5b14282bf2be788381b53
MS <110626902+MSP07@users.noreply.github.com>
2024-05-22 02:26:59 +0530

    Create MajorityElement2.cpp

---
+++ MajorityElement2.cpp
@@ -0,0 +1,17 @@
+class Solution{
+public:
+    vector<int> majorityElement(vector<int>& nums){
+        int n = nums.size()/3;
+        unordered_map<int,int>mpp;
+        vector<int>ans;
+        for(int i=0;i<nums.size();i++){
+            mpp[nums[i]]++;
+        }
+        for(auto&it:mpp){
+            if(it.second>n){
+                ans.push_back(it.first);
+            }
+        }
+        return ans;
+    }
+};
