1fb688ebe6dd46196df5104512879b17a2749ec5
MSP <110626902+MSP07@users.noreply.github.com>
2024-07-21 11:08:04 +0530

    Update VowelsGameInAString.cpp

--- VowelsGameInAString.cpp
+++ VowelsGameInAString.cpp
@@ -13,6 +13,8 @@ private:
     }
 public:
     bool doesAliceWin(string s){
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
         int cnt = 0;
         vowelCnt(s,cnt);
         return cnt != 0;
