5423f13b71760c3c33ace5b08f9e9df32187c19b
MSP <110626902+MSP07@users.noreply.github.com>
2024-09-30 02:32:41 +0530

    Create DetectCapitals.cpp

---
+++ DetectCapitals.cpp
@@ -0,0 +1,15 @@
+/*approach
+  we just follow the given conditions
+  and execute them*/
+class Solution{
+public:
+    bool detectCapitalUse(string word){
+        if(all_of(word.begin(),word.end(),::isupper)||all_of(word.begin(),word.end(),::islower)){
+            return true;
+        }
+        if(isupper(word[0]) && all_of(word.begin()+1,word.end(),::islower)){
+            return true;
+        }
+        return false;
+    }
+};
