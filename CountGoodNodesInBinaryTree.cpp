2153634542fadb3a2e1d41c8c35ec571ddf1fb0d
MSP <110626902+MSP07@users.noreply.github.com>
2024-11-07 01:39:51 +0530

    Update CountGoodNodesInBinaryTree.cpp

--- CountGoodNodesInBinaryTree.cpp
+++ CountGoodNodesInBinaryTree.cpp
@@ -2,19 +2,16 @@
   dfs
   we keep track of highest element encountered till now by prev and by default given root is in count
   then we first calculate all the good nodes in left subtree then right subtree in either tree the 
-  logiv is same we first check if curr val is greater than prev is so we incr count and update prev to that val
-  else if curr val is >= prev then there a large node so we incr count but since values are same making prev = curr->val
-  is waste of time so we simply incr count alone and finally return the count*/
+  logiv is same we check if curr val is greater than prev is so we incr count and update prev to that val
+  and finally return the count*/
 class Solution{
     int count = 1;
 private:
     void dfs(TreeNode* root,int prev){
         if(!root)return;
-        if(root->val > prev){
+        if(root->val >= prev){
             ++count;
             prev = root->val;
-        }else if(root->val>=prev){
-            ++count;
         }
         dfs(root->left,prev);
         dfs(root->right,prev);
