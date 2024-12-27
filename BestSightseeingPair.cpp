fafe1ab4ddbf8d3135a7e4b891aa6e06d4a46511
MSP <110626902+MSP07@users.noreply.github.com>
2024-12-27 22:14:32 +0530

    Update BestSightseeingPair.cpp

--- BestSightseeingPair.cpp
+++ BestSightseeingPair.cpp
@@ -3,10 +3,14 @@
   we intialise max answer to be values[0] + 0 as values[i] + i
   and we update the maxScore to maxVal + values[j] - j or maxScore 
   whichever is maximum. we keep on updating maxVal to max of maxVal
-  or values[j] + j and finally return maxScore*/
+  or values[j] + j and finally return maxScore
+  optimal approach
+  instead of remembering index we can simply remember the value
+  and calculate the max one and finally return it*/
 class Solution{
 public:
     int maxScoreSightseeingPair(vector<int>& values){
+        //base approach
         ios_base::sync_with_stdio(0);
         cin.tie(0);
         int maxVal = values[0] + 0;
@@ -16,5 +20,14 @@ public:
             maxVal = max(maxVal,values[j]+j);
         }
         return maxScore;
+        //optimal approach
+        int a = values[0];
+        int ans = 0;
+        for(int i=1;i<values.size();i++){
+            a--;
+            ans = max(ans,a+values[i]);
+            a = max(a,values[i]);
+        }
+        return ans;
     }
 };
