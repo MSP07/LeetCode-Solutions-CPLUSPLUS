c6d555b4628cce31f7ed9faca82f538c904078e2
MSP <110626902+MSP07@users.noreply.github.com>
2025-01-08 16:40:58 +0530

    Create StringMatchingInAnArray.cpp

---
+++ StringMatchingInAnArray.cpp
@@ -0,0 +1,19 @@
+/*approach
+  string, string matching
+  we use a set and find whether the words inside are subarrays
+  if so we add them to ans and break to prevent duplicates*/
+class Solution{
+public:
+    vector<string> stringMatching(vector<string>& words){
+        vector<string>ans;
+        for(int i=0;i<words.size();i++){
+            for(int j=0;j<words.size();j++){
+                if(i!=j && words[j].find(words[i])!=string::npos){
+                    ans.push_back(words[i]);
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
