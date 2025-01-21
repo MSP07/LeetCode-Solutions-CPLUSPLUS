8183fecdfe3ec063ff478f36f8b72e77ea5870c6
MSP <110626902+MSP07@users.noreply.github.com>
2025-01-22 02:17:22 +0530

    Create GridGame.cpp

---
+++ GridGame.cpp
@@ -0,0 +1,18 @@
+/*approach
+  matrix, prefix sum
+  using the pre computed sum and the rules given
+  we calculate the answer*/
+class Solution {
+public:
+    long long gridGame(vector<vector<int>>& grid) {
+        long long firstRowSum = accumulate(begin(grid[0]), end(grid[0]), 0LL),
+                  secondRowSum = 0;
+        long long minimumSum = LONG_LONG_MAX;
+        for (int turnIndex = 0; turnIndex < grid[0].size(); ++turnIndex) {
+            firstRowSum -= grid[0][turnIndex];
+            minimumSum = min(minimumSum, max(firstRowSum, secondRowSum));
+            secondRowSum += grid[1][turnIndex];
+        }
+        return minimumSum;
+    }
+};
