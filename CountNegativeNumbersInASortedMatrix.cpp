3b3b6e7995b775b6256c8756348ed95ae3c33485
MSP <110626902+MSP07@users.noreply.github.com>
2025-02-16 05:34:54 +0530

    Create CountNegativeNumbersInASortedMatrix.cpp

---
+++ CountNegativeNumbersInASortedMatrix.cpp
@@ -0,0 +1,19 @@
+/*approach
+  binary search
+  idea is to find negative number in descending sorted matrix
+  and we use binary search to find the num less than 0 and take a 
+  count of that and finally return it*/
+class Solution {
+public:
+    int countNegatives(vector<vector<int>>& grid) {
+        int cnt = 0;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]<0){
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+};
